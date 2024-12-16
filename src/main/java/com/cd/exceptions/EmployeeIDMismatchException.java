a99772903a6ece96e446c6dbe8c405b736861253
pratik.dimble <pratik.dimble@nitorinfotech.com>
2024-12-16 10:46:02 +0530

    Initial Commit

--- src/main/java/com/cd/exceptions/EmployeeIDMismatchException.java
+++
@@ -1,9 +0,0 @@
-package com.cd.exceptions;
-
-public class EmployeeIDMismatchException extends  RuntimeException{
-
-/*    public EmployeeIDMismatchException(){
-        super(message);
-    }*/
-
-}
--- src/main/java/com/cd/exceptions/EmployeeNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package com.cd.exceptions;
-
-public class EmployeeNotFoundException extends RuntimeException {
-}
